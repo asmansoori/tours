19d2c79405648501a247a7c68c14b2a965fb6e6d
asmansoori1 <salar.mansoori.2003@gmail.com>
2025-06-29 22:29:55 +0200

    Modify tour.js to toggle more and less text

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Tour({ id, image, info, price, name}) {
-  return (
-    <article className='single-tour'>
-        <img src={image} alt={name}/>
-        <footer>
-            <div className='tour-info'>
-                <h4>{name}</h4>
-                <h4 className='tour-price'>${price}</h4>
-            </div>
-            <p>{info}</p>
-            <button className='delete-btn'>not interested</button>
-        </footer>
-    </article>
-  )
+    const [readMore, setReadMore] = useState(false)
+
+    return (
+        <article className='single-tour'>
+            <img src={image} alt={name}/>
+            <footer>
+                <div className='tour-info'>
+                    <h4>{name}</h4>
+                    <h4 className='tour-price'>${price}</h4>
+                </div>
+                <p>
+                    {
+                    readMore ? info : info.substring(0, info.length/2)
+                    }...
+                    <button onClick={() => setReadMore(!readMore)}>{readMore ? 'show less' : 'show more'}</button>
+                </p>
+                <button className='delete-btn'>not interested</button>
+            </footer>
+        </article>
+    )
 }
 
 export default Tour
