acf19df82aee6bc608eaead57c52e57f5e17ccff
asmansoori1 <salar.mansoori.2003@gmail.com>
2025-06-29 21:16:33 +0200

    Firt commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react'
+
+
+const url = 'https://course-api.com/react-tours-project'
+function App() {
+  return <h2>Tours Project Setup</h2>
+}
+
+export default App
